8c25a53ea2a3b5c31f5d80e3ce6e3aee02023027
piyush2931 <piyushj572@gmail.com>
2020-08-07 12:03:56 +0530

    email modification

--- email.php
+++ email.php
@@ -1,18 +1,23 @@
 
 
-<?php
-
-if($_POST["submit"]) {
-    $recipient="piyushj572@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["name"];
-    $senderEmail=$_POST["email"];
+<?php 
+if(isset($_POST['submit'])){
+    $to = "piyushj572@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
     $phone_number = $_POST['number'];
-    $message=$_POST["comments"];
+    $message = $_POST['comments'];
+    $subject = "Form submission";
+    $email_body = "You have received a new message. ".
+        " Here are the details:\n Name: $name \n ".
+        "Email: $from\n Phone Number: $phone_number\n Message: \n $message";
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\nPhone Number: $phone_number\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-}
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$email_body,$headers);
+    
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
